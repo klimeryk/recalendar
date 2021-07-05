f24c0066b353f9d405f97b70be23612d50cedd7e
Igor Klimer <github@gpg.klimer.eu>
2021-07-05 19:24:10 +0000

    Make sure the local config is optional

--- generate.php
+++ generate.php
@@ -2,7 +2,9 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/local.config.php';
+if ( file_exists( __DIR__ . '/local.config.php' ) ) {
+	require_once __DIR__ . '/local.config.php';
+}
 
 if ( class_exists( '\ReCalendar\LocalConfig' ) ) {
 	$config = new \Recalendar\LocalConfig();
